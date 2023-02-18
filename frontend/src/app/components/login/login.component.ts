e62120df89dccbae5dccf9a62dc4d680ed19d658
FabianReyna <3753836700101@ingenieria.usac.edu.gt>
2023-02-18 11:53:13 -0600

    Creacion de login
    Se creo el componente para el login, con la conexion a la api base

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendService } from 'src/app/services/backend.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private backend: BackendService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  cuerpo: any = {
+    Usuario: '',
+    Password: ''
+  }
+
+  Login() {
+    if (this.cuerpo.Usuario == "" || this.cuerpo.Password == "") {
+      alert("Complete todos los campos")
+      return;
+    }
+    this.backend.Login(this.cuerpo).subscribe(
+      res => {
+        const resp = JSON.parse(JSON.stringify(res))
+        if (resp.Res) {
+          sessionStorage.setItem("usuario", this.cuerpo.Usuario)
+          this.cuerpo.Usuario = ""
+          this.cuerpo.Password = ""
+          //this.router.navigate(['inicio'])
+        } else {
+          alert("Usuario o Contraseña Incorrecta")
+          this.cuerpo.Usuario = ""
+          this.cuerpo.Password = ""
+        }
+      },
+      err => {
+        alert("Ocurrio un error :(")
+      }
+    )
+  }
+
+}
