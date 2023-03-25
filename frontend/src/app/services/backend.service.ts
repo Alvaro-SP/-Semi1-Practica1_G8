759898fc5628f8158e9807a75c23073fbd86479d
FabianReyna <3753836700101@ingenieria.usac.edu.gt>
2023-03-25 11:34:46 -0600

    version 2.0,0

--- frontend/src/app/services/backend.service.ts
+++ frontend/src/app/services/backend.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class BackendService {
 
   constructor(private http: HttpClient) { }
-  URL: string = "http://localhost:8080"
+  URL: string = "http://3.17.81.208:8080"
 
   Login(cuerpo: any) {
     return this.http.post(`${this.URL}/login`, cuerpo);
